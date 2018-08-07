a1e531cc8e2c4e2581c9d3860e9f005e4b469da1
Tirem Bareno-Sosna <tirempbs@gmail.com>
2018-08-07 18:23:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,11 +17,5 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
-  function incrementVariable() {
-  i = i + 1;
-}
-  do {
-    array.pop();
-  } while (array.length > 0 && incrementVariable());
-  return array;
+  
 }
